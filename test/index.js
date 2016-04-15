0fa7b0647b420c645a7a1984b90c443b4e95f2ed
Ryan Tsao <rtsao@uber.com>
2016-04-14 22:13:53 -0700

    Added test for line terminators

--- test/index.js
+++ test/index.js
@@ -8,3 +8,8 @@ test('escapes HTML characters', function(t) {
   t.equal(safeString('</script>'), '\\u003C\\u002Fscript\\u003E');
   t.end();
 });
+
+test('escapes line terminators', function(t) {
+  t.equal(safeString('\u2028\u2029'), '\\u2028\\u2029');
+  t.end();
+});
